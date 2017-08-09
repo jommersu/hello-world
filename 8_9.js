b32539c896771e48414a61c99f8242c16a4c751c
jommersu <jommer.su@foxmail.com>
2017-08-09 14:27:10 +0800

    8_9

--- 8_9.js
+++ 8_9.js
@@ -1,8 +1,10 @@
 let arr = [0 ,1 ];
-let N = 10;
+let N = 11;
+function  Fibonacci(N){
 for(let i = 2; i < N  ; i++){
     arr[i] = arr[i-2] + arr[i-1];
 }
-console.log(arr);
-console.log("第"+N+"个数字是："+ arr[N-1]);
+return arr[N-1];
+}
+console.log("第"+N+"个数字是："+ Fibonacci(N));
 
