b09238406a50870cf746cfbe31081c11eb67704b
jommersu <jommer.su@foxmail.com>
2017-08-11 14:39:19 +0800

    8_11

--- 8_11.js
+++ 8_11.js
@@ -1,15 +1,15 @@
 let str = "1->2->3->3->4->5->3";
 let val = 3;
-let chain = delete_three(str);
+let chain = delete_three(str,val);
 console.log("原始链表：" + str);
-console.log("删除链表中的3：" + chain);
+console.log("删除链表中的"+val+"：" + chain);
 function delete_three(str,val){
     let result_chain = "";
     let result_arr = [];
     let arr = [];
     arr = str.split("->");
     for(let item of arr){
-        if(item !== '3'){
+        if(item !== (''+val)){
             result_arr.push(item);
         }
     }
