aea817e3dd4f62a3f9aa48b2db423f9150409690
jommersu <jommer.su@foxmail.com>
2017-08-11 14:23:57 +0800

    8_11

---
+++ 8_11.js
@@ -0,0 +1,21 @@
+let str = "1->2->3->3->4->5->3";
+let val = 3;
+let chain = delete_three(str);
+console.log(chain);
+function delete_three(str,val){
+    let result_chain = "";
+    let result_arr = [];
+    let arr = [];
+    arr = str.split("->");
+    for(let item of arr){
+        if(item !== '3'){
+            result_arr.push(item);
+        }
+    }
+    let len = result_arr.length;
+    for(let i = 0; i < len - 1; i ++){
+        result_chain += result_arr[i] + "->";
+    }
+    result_chain += result_arr[len - 1];
+    return result_chain;
+}
