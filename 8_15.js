1dfe54e921dc83dd270c3390ec393903d724b37b
jommersu <jommer.su@foxmail.com>
2017-08-15 23:39:40 +0800

    8_15_modified

--- 8_15.js
+++ 8_15.js
@@ -1,6 +1,21 @@
 let arr = [1,3,7,1,4];
 let item = 1;
-let result = [];
+let count = 0;
+arr.map(function (element) {
+    if(item === element){
+        count ++;
+    }
+    return count;
+})
+console.log(item + "出现的次数为" + count);
+
+let num = arr.filter(ele => {
+    return ele === item;
+    }).length;
+console.log(item + "出现的次数为" + num);
+
+
+/*let result = [];
 let count = 0;
 arr.forEach(function(element) {
     if(element === item){
@@ -8,3 +23,4 @@ arr.forEach(function(element) {
     }
 });
 console.log(item + "出现的次数为" + count);
+*/
